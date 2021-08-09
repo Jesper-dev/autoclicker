972028ec9cda788bcdf0fd43dcd8fa63e8461ab4
JesperKYH <jesper.pettersson@student.kyh.se>
2021-08-09 09:30:59 +0200

    Adding function for creating the square

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,21 +1,32 @@
 import { useState } from 'react';
+import { CreateSquare } from '../types/modules/square';
 import { Player } from '../types/classes/playerClass';
 
 const HomePage = () => {
-  const [state, setState] = useState<{ startGame: boolean; score: number }>({
+  const [state, setState] = useState<{
+    startGame: boolean;
+    score: number;
+    square: JSX.Element | null;
+  }>({
     startGame: false,
     score: 0,
+    square: null,
   });
   let player = new Player(state.score, 'Jesper');
   const sqaureClick = () => {
     player.addScore();
     setState((prevState) => ({ ...prevState, score: player.score }));
-    console.log(player.score);
   };
 
   const startGame = () => {
     player.name = 'Jesper King';
     setState((prevState) => ({ ...prevState, startGame: true }));
+    setState((prevState) => ({
+      ...prevState,
+      square: CreateSquare(
+        <div className="square" onClick={() => sqaureClick()}></div>
+      ),
+    }));
     console.log(player);
   };
   return (
@@ -23,11 +34,9 @@ const HomePage = () => {
       <h1 className="score" unselectable="on">
         {state.score}
       </h1>
-      {state.startGame ? (
-        <div className="square" onClick={() => sqaureClick()}></div>
-      ) : (
-        <button onClick={() => startGame()}>Click Me</button>
-      )}
+      <button onClick={() => startGame()}>Click Me</button>
+      {/* The sqaure that spans after we click 'start game' button */}
+      {state.square}
     </section>
   );
 };
--- src/types/modules/square.ts
+++ src/types/modules/square.ts
@@ -1,8 +1,14 @@
-//contextual typing. This helps cut down on the amount of effort to keep your program typed.
-export const addScore: (number: number) => number = function (number) {
-  return (number += 1)
-}
+import { SqaureProps } from '../interfaces';
 
-export const addScoreRobot: (number: number) => number = function (number) {
-  return (number += 3)
-}
+export const CreateSquare = (children: JSX.Element) => {
+  return children;
+};
+
+//contextual typing. This helps cut down on the amount of effort to keep your program typed.
+// export const addScore: (number: number) => number = function (number) {
+//   return (number += 1)
+// }
+
+// export const addScoreRobot: (number: number) => number = function (number) {
+//   return (number += 3)
+// }
